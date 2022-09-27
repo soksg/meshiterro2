4f474632b217db9c32b9233cbf1d02e1ee2fb8c6
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-27 23:59:54 +0000

    [Add]4-24/fontawesome追加

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative "application"
 
+const webpack=require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+    })
+   )
+
 # Initialize the Rails application.
 Rails.application.initialize!
